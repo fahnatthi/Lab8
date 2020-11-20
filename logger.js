0cebf4b41ec05ad8671619ffe43dc1c106c3151f
fhutzchx <fahut.ittiwat@gmail.com>
2020-11-21 01:02:14 +0700

    add sentry

--- logger.js
+++ logger.js
@@ -3,7 +3,8 @@ const winston = require('winston');
 const { Timber } = require("@timberio/node")
 const { TimberTransport } = require("@timberio/winston")
 
-const timber = new Timber(eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2FwaS50aW1iZXIuaW8vIiwiZXhwIjpudWxsLCJpYXQiOjE2MDU4OTMxNDgsImlzcyI6Imh0dHBzOi8vYXBpLnRpbWJlci5pby9hcGlfa2V5cyIsInByb3ZpZGVyX2NsYWltcyI6eyJhcGlfa2V5X2lkIjoxMTEzMywidXNlcl9pZCI6ImFwaV9rZXl8MTExMzMifSwic3ViIjoiYXBpX2tleXwxMTEzMyJ9.IBhQY3pJxsQVW5NqopUrgNwTL6U0KN9dOVCOw5PDja4)
+const timber = new Timber(eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2FwaS50aW1iZXIuaW8vIiwiZXhwIjpudWxsLCJpYXQiOjE2MDU4OTUwNTYsImlzcyI6Imh0dHBzOi8vYXBpLnRpbWJlci5pby9hcGlfa2V5cyIsInByb3ZpZGVyX2NsYWltcyI6eyJhcGlfa2V5X2lkIjoxMTI0MCwidXNlcl9pZCI6ImFwaV9rZXl8MTEyNDAifSwic3ViIjoiYXBpX2tleXwxMTI0MCJ9.yT6FrB6CisFZ8qokJgxLx-E-Ol-tlIy1CuoKjTOdgkY
+  )
 
 const logger = createLogger({
   level: 'info',
